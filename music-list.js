4dbeb0fa65522a7ac86ff6452c280b173f9180bb
Tín Lưu <107022820+tinluuVTL@users.noreply.github.com>
2022-06-14 18:33:10 +0700

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,54 @@
+// Để thêm bài nhạc mình sẽ them vào list sau
+
+//   {
+//     name: "Tên bài nhạc",
+//     artist: "Nghệ sĩ",
+//     img: "Tên ảnh định dạng .jpg trong project",
+//     src: "Link đường dẫn file nhạc đinh dạng .mp3"
+//   }
+
+let allMusic = [
+  {
+    name: "Đám Cưới Nha",
+    artist: "Hồng Thanh,Mie",
+    img: "Dam-Cuoi-Nha",
+    src: "Dam-cuoi-nha"
+  },
+ 
+  {
+    name: "See Tình",
+    artist: "Hoàng Thùy Linh",
+    img: "See-Tinh",
+    src: "See-Tinh"
+  },
+  {
+    name: "Chỉ Muốn Bên Em Lúc Này",
+    artist: "Huy Vạc",
+    img: "ChiMuonBenEmLucNay",
+    src: "ChiMuonBenEmLucNay"
+  },
+  {
+    name: "Hồng Nhan",
+    artist: "Jack",
+    img: "Hong-Nhan-Jack",
+    src: "Hong-Nhan-Jack"
+  },
+  {
+    name: "Chỉ Muốn Bên Em Thật Gần",
+    artist: "YLinh,Xám",
+    img: "Chi-Muon-Ben-Em-That-Gan",
+    src: "Chi-Muon-Ben-Em-That-Gan"
+  },
+  {
+    name: "Em Là Nhất",
+    artist: "ssss",
+    img: "Em-La-Nhat",
+    src: "Em-La-Nhat"
+  },
+  {
+    name: "Vì anh là gió",
+    artist: "Lilk",
+    img: "ViAnhLaGio-LilK-4766315",
+    src: "ViAnhLaGio-LilK-4766315"
+  },
+];
